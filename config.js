33e0827029f4e72a3fb9e2116c43ef7c3d168dcc
beCoditive <73736326+beCoditive@users.noreply.github.com>
2021-02-09 22:12:22 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -3,11 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.dbName = exports.owners = exports.prefix = exports.token = void 0;
 exports.token = 'ODA4NjEyNjY0Nzg2OTQ0MDAw.YCJFOg.fvpqpn5Ows70PFJ_udcYb81Qt8w';
 exports.prefix = '$';
-exports.version = 'v1.7.6';
+exports.version = 'v1.6.9';
 exports.defaultRole = '801322780833349643';
 exports.serverName = 'beCoditive'
 exports.quotesChannel = '804585106395889694'
 exports.serverLink = 'https://discord.gg/5JtJFEcZeP'
 exports.welcomeImage = 'https://media.discordapp.net/attachments/771230051336716319/801795688170127391/Untitled-1.png'
 exports.owners = ['760922441563373618'];
-exports.dbName = 'database'
