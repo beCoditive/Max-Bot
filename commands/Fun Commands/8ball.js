d1e309f94e34936fcb266cda45e2a69f0120ecf2
DevShaded <devshaded@gmail.com>
2021-02-10 21:13:17 +0100

    Edit: Changed the 8ball command a little and cleaned some code

--- commands/Fun Commands/8ball.js
+++ commands/Fun Commands/8ball.js
@@ -1,4 +1,4 @@
-"use strict";
+    "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const discord_akairo_1 = require("discord-akairo");
 const { MessageEmbed } = require("discord.js");
@@ -22,14 +22,36 @@ class ballCommand extends discord_akairo_1.Command {
        let question = message.content.slice(7)
        if(!question) return message.channel.send('Please ask a Full Question')
 
-       let answers = ['Yes' , 'No' , 'Why Not' , 'Not in a million Years' , 'No Comments' , 'Definetly']
+       let answers = [
+           'It is certain',
+           'It is decidedly so',
+           'Without a doubt',
+           'Yes – definitely',
+           'You may rely on it',
+           'As I see it, yes',
+           'Most likely',
+           'Outlook good',
+           'Yes',
+           'Signs point to yes',
+           'Reply hazy, try again',
+           'Ask again later',
+           'Better not tell you now',
+           'Cannot predict now',
+           'Concentrate and ask again',
+           'Don\'t count on it',
+           'My reply is no',
+           'My sources say no',
+           'Outlook not so good',
+           'Very doubtful',
+       ]
 
        let response = answers[Math.floor(Math.random() * answers.length)]
+
        let Embed = new MessageEmbed()
-       .setTitle('8ball!')
        .setColor('RANDOM')
-       .setDescription(`Your Question: ${question}\nMy Reply: ${response}`)
-       message.channel.send(Embed)
+       .setDescription(`🎱 <@${message.author.id}>, ${response}`)
+       .setFooter(`Original Message: ${question}`)
+       await message.channel.send(Embed)
     }
 }
 exports.default = ballCommand;
